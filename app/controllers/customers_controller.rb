3d3815ce9d94cf7879c0dcbd8de3084558dffe2e
denimcouch <alexmata92@ymail.com>
2020-09-28 17:19:22 -0500

    built crud functionality for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,53 @@
 class CustomersController < ApplicationController
+    before_action :current_customer, only: [:show, :edit, :update, :destroy]
+
+    def current_customer
+        @customer = Customer.find(params[:id])
+    end
+
+    def index
+        @customers = Customer.all
+    end
+    
+    def show
+    end
+
+    def new
+        @customer = Customer.new
+    end
+
+    def create
+        @customer = Customer.new(custy_params)
+        if @customer.valid?
+            @customer.save
+            redirect_to customer_path(@customer)
+        else
+            flash[:errors] = @customer.errors.full_messages
+            redirect_to new_customer_path
+        end
+    end
+
+    def edit 
+    end
+
+    def update
+        @customer.update(custy_params)
+        if @customer.valid?
+            redirect_to customer_path(@customer)
+        else
+            flash[:errors] = @customer.errors.full_messages
+            redirect_to edit_customer_path(@customer)
+        end
+    end
+
+    def destroy
+        @customer.destroy
+        redirect_to customers_path
+    end
+
+    private
+
+    def custy_params
+        params.require(:customer).permit(:first_name, :last_name)
+    end
 end
