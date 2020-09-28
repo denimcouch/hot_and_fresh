467359f06e1cbecab9f6200bc3a5384a3f4cbe47
denimcouch <alexmata92@ymail.com>
2020-09-28 15:35:08 -0500

    populated seed data

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+    has_many :recipes
+    has_many :bakeries, through: :recipes
+    has_many :orders
+    has_many :customers, through: :orders
+    
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+    belongs_to :item
+    belongs_to :bakery
 end
