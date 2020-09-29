295ff40c93f7ffe729461c3541b51b0daddd817f
denimcouch <alexmata92@ymail.com>
2020-09-29 14:02:44 -0500

    Built index and show routes for Item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+    def index
+        @items = Item.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    #We want to create a new item. That will be done via recipes#new.
 end
--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,4 +1,10 @@
 class Bakery < ApplicationRecord
     has_many :recipes
     has_many :items, through: :recipes
+
+    def most_purchased
+        self.items.sort{ |item|
+            item.orders.count
+        }.reverse[0]
+    end
 end
