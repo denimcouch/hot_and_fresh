cf8e8747eb43ff955b3598a04ee659f7a82b0697
denimcouch <alexmata92@ymail.com>
2020-09-29 17:33:35 -0500

    created form for New Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+    
+    def index
+        @orders = Order.all
+    end
+
+    def new
+        # byebug
+        @customers = Customer.all
+        @bakery = Bakery.find(params[:bakery])
+        @items = @bakery.items
+    end
+
+    def create
+        # byebug
+        params[:item_id].each do |item_id|
+            @order = Order.new
+            @order.customer_id = params[:customer_id]
+            @order.item_id = item_id
+            @order.save
+        end
+        redirect_to customer_path(params[:customer_id])
+    end
+
+    def destroy
+
+    end
 end
