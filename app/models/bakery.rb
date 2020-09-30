020e26260dcf9ab4b89c3af7094134c44a22ba87
denimcouch <alexmata92@ymail.com>
2020-09-30 16:45:55 -0500

    added more helper methods

--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,12 +1,25 @@
 class Bakery < ApplicationRecord
     has_many :recipes
     has_many :items, through: :recipes
+    has_many :orders
+    has_many :customers, through: :orders
 
-    def most_purchased
+    def most_popular
         self.items.sort{ |item|
             item.orders.count
         }.reverse[0]
     end
+    
+    def most_valued_customer
+        (self.customers.max_by{|c| c.orders.count})
+    end
 
+    def most_valued_customer_orders
+        self.most_valued_customer.orders.select{|order| order.bakery_id == self.id}
+    end
+
+    def total_sales
+        (self.orders.collect{|order| order.item}).sum{|item| item.price}
+    end
     
 end
