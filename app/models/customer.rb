3640028b8aa56353f4fddeb8a8985cc4f9c17909
denimcouch <alexmata92@ymail.com>
2020-09-30 10:04:27 -0500

    created order history helpers for Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,15 @@ class Customer < ApplicationRecord
     def full_name
         self.first_name + " " + self.last_name
     end
+
+    def orders_by_date
+        self.orders.sort_by{ |orders|
+            orders.created_at
+        }.reverse
+    end
+
+    def order_history
+        self.orders_by_date.group_by{|o| o.created_at.strftime("%D")}
+    end
+
 end
