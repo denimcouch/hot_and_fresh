7c9e4f77545b7df4bf85dbe5f885d402984bfdc2
denimcouch <alexmata92@ymail.com>
2020-09-30 11:59:09 -0500

    altered order form to accept bakery id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,40 @@
 class OrdersController < ApplicationController
     
+    
     def index
         @orders = Order.all
     end
 
     def new
         # byebug
+        @order = Order.new
         @customers = Customer.all
+        @bakeries = Bakery.all
         @bakery = Bakery.find(params[:bakery])
         @items = @bakery.items
     end
 
     def create
         # byebug
-        params[:item_id].each do |item_id|
+        items_list = (params[:order][:item_id][1..:item_id.length])
+        items_list.each do |item_id|
             @order = Order.new
-            @order.customer_id = params[:customer_id]
+            @order.customer_id = params[:order][:customer_id]
             @order.item_id = item_id
+            @order.bakery_id = params[:order][:bakery_id]
             @order.save
         end
-        redirect_to customer_path(params[:customer_id])
+        redirect_to customer_path(@order.customer_id)
     end
 
     def destroy
 
     end
+
+    private
+
+    def order_params
+        params.require(:order).permit( :customer_id, :bakery_id, :item_id => [])
+    end
+
 end
